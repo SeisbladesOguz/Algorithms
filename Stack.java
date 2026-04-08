9d55b9e173420ae4811087e9978da8978f33a0dd
Oguz <bhdrogz7@gmail.com>
2026-04-08 21:28:18 +0300

    Add InfixToPostfix algorithm

---
+++ Stack.java
@@ -0,0 +1,64 @@
+package InfixToPostfix;
+
+
+
+public class Stack<T> {
+
+	private T[] array; // Object yerine direkt T[] kullanıyoruz
+	private int top;
+	private int capacity;
+
+	@SuppressWarnings("unchecked")
+	public Stack(int size) {
+		this.capacity = size;
+		this.array = (T[]) new Object[capacity];
+		this.top = -1;
+	}
+
+	public void push(T item) {
+		if (isFull()) {
+			throw new IllegalStateException("Stack Overflow!");
+		}
+
+		top++;
+
+		array[top] = item;
+	}
+
+	public T pop() {
+		if (isEmpty()) {
+			throw new IllegalStateException("Stack Underflow!");
+		}
+
+		T a = array[top];
+
+		top--;
+
+		return a;
+
+	}
+
+	public boolean isEmpty() {
+
+		if (top == -1) {
+
+			return true;
+
+		}
+
+		return false;
+
+	}
+
+	public boolean isFull() {
+
+		if (top == capacity) {
+
+			return true;
+
+		}
+
+		return false;
+
+	}
+}
